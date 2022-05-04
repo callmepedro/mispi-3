3e45c3f7072cdada64dc5f1814f351b3b19b46e8
callmepedro <callmepedro@mail.ru>
2022-05-04 13:34:48 +0300

    Do report target

--- src/test/java/Beans/HitsDataTest.java
+++ src/test/java/Beans/HitsDataTest.java
@@ -8,6 +8,6 @@ public class HitsDataTest {
     @Test
     public void test() {
         boolean result = HitsData.methodForTest(1);
-        assertEquals(1, 0);
+        assertEquals(result, true);
     }
 }
